3eec106824758640738728fb6557cf5c8932e67d
Tim Vu <timbim8996@gmail.com>
2024-01-18 21:53:40 -0800

    added request() and partial implementation of release()

--- CS143B-Project1/src/rcb.java
+++ CS143B-Project1/src/rcb.java
@@ -1,33 +1,37 @@
 public class rcb {
     
-    int owner, units;
-    String name;
+    int units, id;
+    int[] owners;
     int[] waitList;
 
-    public rcb(int units, String name) {
+    public rcb(int units, int id) {
 
-        this.owner = -1;        //by default, set to -1 for no owner
         this.units = units;
-        this.name = name;
+        this.id = id;
+        owners = new int[units];     //there can only be as many owners as there are units
         waitList = new int[15];      //there are 16 total processes, so edge case, all 16 processes request a single resource (1 holds it while the other 15 wait)
+
+        for(int i = 0; i < 15; i++) { waitList[i] = -1; }     //set all to -1 to show nothing is on waitlist
+
+        
 
     }
 
     void decrementUnits(int units) { 
-        System.out.println(this.name + " units decrementing by " + units);
+        System.out.println("Units decrementing by " + units);
         this.units -= units; 
     }
     void incrementUnits(int units) { 
-        System.out.println(this.name + " units incrementing by " + units);
+        System.out.println("Units incrementing by " + units);
         this.units += units; 
     }
     int getUnitCount() { return this.units; }
-    
-    void setOwner(int newOwner) { this.owner = newOwner; }
-    int getOwner() { return this.owner; }
 
-    String getName() { return this.name; }
+    int getID() { return this.id; }
 
     int[] getWaitList() { return this.waitList; }
+    int[] getOwners() { return this.owners; }
+
+
 
 }
